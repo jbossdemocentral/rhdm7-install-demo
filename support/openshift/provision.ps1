93b8d2e373aa24837e3d015a58aa49d4cf55a866
Duncan Doyle <Duncan.Doyle@gmail.com>
2018-03-08 12:14:04 +0100

    Fixed type in PowerShell script.

--- support/openshift/provision.ps1
+++ support/openshift/provision.ps1
@@ -16,7 +16,7 @@ param (
 if ((Get-Command "oc" -ErrorAction SilentlyContinue) -eq $null)
 {
    Write-Output "The oc client tools need to be installed to connect to OpenShift."
-   Write-Output "Download it from https://www.opnshift.org/download.html and confirm that \"oc version\" runs.`n"
+   Write-Output "Download it from https://www.openshift.org/download.html and confirm that \"oc version\" runs.`n"
    exit
 }
 
