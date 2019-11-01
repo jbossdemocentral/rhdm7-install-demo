f8cf5430e442ce60fe0035d4bd629799cc9917f3
Duncan Doyle <Duncan.Doyle@gmail.com>
2019-11-01 17:24:20 +0100

    Updated to new RHDM OpenShift image names.

--- support/openshift/provision.ps1
+++ support/openshift/provision.ps1
@@ -108,7 +108,7 @@ $KIE_SERVER_PWD="kieserver1!"
 #OpenShift Template Parameters
 #GitHub tag referencing the image streams and templates.
 $OPENSHIFT_DM7_TEMPLATES_TAG="7.5.0.GA"
-$IMAGE_STREAM_TAG="1.0"
+$IMAGE_STREAM_TAG="7.5.0"
 $DM7_VERSION="75"
 
 ################################################################################
@@ -222,8 +222,8 @@ Function Import-ImageStreams-And-Templates() {
   Start-Sleep -s 10
 
   #  Explicitly import the images. This is to overcome a problem where the image import gets a 500 error from registry.redhat.io when we deploy multiple containers at once.
-  Call-Oc "import-image rhdm$DM7_VERSION-decisioncentral-openshift:$IMAGE_STREAM_TAG —confirm -n $($PRJ[0])" $True "Error fetching Image Streams."
-  Call-Oc "import-image rhdm$DM7_VERSION-kieserver-openshift:$IMAGE_STREAM_TAG —confirm -n $($PRJ[0])" $True "Error fetching Image Streams."
+  Call-Oc "import-image rhdm-decisioncentral-rhel8:$IMAGE_STREAM_TAG —confirm -n $($PRJ[0])" $True "Error fetching Image Streams."
+  Call-Oc "import-image rhdm-kieserver-rhel8:$IMAGE_STREAM_TAG —confirm -n $($PRJ[0])" $True "Error fetching Image Streams."
 
   #Write-Output-Header "Patching the ImageStreams"
   #oc patch is/rhdm73-decisioncentral-openshift --type='json' -p "[{'op': 'replace', 'path': '/spec/tags/0/from/name', 'value': 'registry.access.redhat.com/rhdm-7/rhdm73-decisioncentral-openshift:1.0'}]"
@@ -286,7 +286,7 @@ Function Create-Application() {
 
   # Disable the OpenShift Startup Strategy and revert to the old Controller Strategy
   oc set env dc/$ARG_DEMO-rhdmcentr KIE_WORKBENCH_CONTROLLER_OPENSHIFT_ENABLED=false
-  oc set env dc/$ARG_DEMO-kieserver KIE_SERVER_STARTUP_STRATEGY=ControllerBasedStartupStrategy KIE_SERVER_CONTROLLER_USER=$KIE_SERVER_CONTROLLER_USER KIE_SERVER_CONTROLLER_PWD=$KIE_SERVER_CONTROLLER_PWD KIE_SERVER_CONTROLLER_SERVICE=$ARG_DEMO-rhdmcentr KIE_SERVER_CONTROLLER_PROTOCOL=ws
+  oc set env dc/$ARG_DEMO-kieserver KIE_SERVER_STARTUP_STRATEGY=ControllerBasedStartupStrategy KIE_SERVER_CONTROLLER_USER=$KIE_SERVER_CONTROLLER_USER KIE_SERVER_CONTROLLER_PWD=$KIE_SERVER_CONTROLLER_PWD KIE_SERVER_CONTROLLER_SERVICE=$ARG_DEMO-rhdmcentr KIE_SERVER_CONTROLLER_PROTOCOL=ws KIE_SERVER_ROUTE_NAME=insecure-$ARG_DEMO-kieserver
 
 }
 
--- support/openshift/provision.sh
+++ support/openshift/provision.sh
@@ -166,7 +166,7 @@ KIE_SERVER_PWD=kieserver1!
 
 # Version Configuration Parameters
 OPENSHIFT_DM7_TEMPLATES_TAG=7.5.0.GA
-IMAGE_STREAM_TAG=1.0
+IMAGE_STREAM_TAG=7.5.0
 DM7_VERSION=75
 
 
@@ -254,8 +254,8 @@ function import_imagestreams_and_templates() {
   runSpinner 10
 
   #  Explicitly import the images. This is to overcome a problem where the image import gets a 500 error from registry.redhat.io when we deploy multiple containers at once.
-  oc import-image rhdm$DM7_VERSION-decisioncentral-openshift:$IMAGE_STREAM_TAG —confirm -n ${PRJ[0]}
-  oc import-image rhdm$DM7_VERSION-kieserver-openshift:$IMAGE_STREAM_TAG —confirm -n ${PRJ[0]}
+  oc import-image rhdm-decisioncentral-rhel8:$IMAGE_STREAM_TAG —confirm -n ${PRJ[0]}
+  oc import-image rhdm-kieserver-rhel8:$IMAGE_STREAM_TAG —confirm -n ${PRJ[0]}
 
   #echo_header "Patching the ImageStreams"
   #oc patch is/rhdm73-decisioncentral-openshift --type='json' -p '[{"op": "replace", "path": "/spec/tags/0/from/name", "value": "registry.access.redhat.com/rhdm-7/rhdm73-decisioncentral-openshift:1.0"}]'
@@ -341,7 +341,7 @@ function create_application() {
 
     # Disable the OpenShift Startup Strategy and revert to the old Controller Strategy
     oc set env dc/$ARG_DEMO-rhdmcentr KIE_WORKBENCH_CONTROLLER_OPENSHIFT_ENABLED=false
-    oc set env dc/$ARG_DEMO-kieserver KIE_SERVER_STARTUP_STRATEGY=ControllerBasedStartupStrategy KIE_SERVER_CONTROLLER_USER=$KIE_SERVER_CONTROLLER_USER KIE_SERVER_CONTROLLER_PWD=$KIE_SERVER_CONTROLLER_PWD KIE_SERVER_CONTROLLER_SERVICE=$ARG_DEMO-rhdmcentr KIE_SERVER_CONTROLLER_PROTOCOL=ws
+    oc set env dc/$ARG_DEMO-kieserver KIE_SERVER_STARTUP_STRATEGY=ControllerBasedStartupStrategy KIE_SERVER_CONTROLLER_USER=$KIE_SERVER_CONTROLLER_USER KIE_SERVER_CONTROLLER_PWD=$KIE_SERVER_CONTROLLER_PWD KIE_SERVER_CONTROLLER_SERVICE=$ARG_DEMO-rhdmcentr KIE_SERVER_CONTROLLER_PROTOCOL=ws KIE_SERVER_ROUTE_NAME=insecure-$ARG_DEMO-kieserver
 
 }
 
